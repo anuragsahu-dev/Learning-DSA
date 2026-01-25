9e8c94218a526534b7fb4a0736eafd0988433761
Anurag Sahu <as3305100@gmail.com>
2026-01-26 00:06:57 +0530

    hello

--- 06-Linked-Linked-Easy-Medium/13-remove-nth-Node-from end-One-Pass.js
+++ 06-Linked-Linked-Easy-Medium/13-remove-nth-Node-from end-One-Pass.js
@@ -22,3 +22,34 @@
 // Time Complexity: O(n)
 // Space Complexity: O(1)
 
+
+
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+// approach 2 using two pointer p1 and p2.
+// p1 pointer start from sentinel node.
+// p2 pointer start from p1 + n
+// both p1 and p2 moves 1 step one time 
+
+function removeNthFromEnd(head, n) {
+    let sentinel = new ListNode(0, head);
+
+    let p2 = sentinel;
+
+    for (let i = 0; i < n; i++) {
+        p2 = p2.next;
+    }
+
+    let p1 = sentinel;
+
+    while (p2.next) {
+        p1 = p1.next;
+        p2 = p2.next;
+    }
+
+    p1.next = p1.next.next;
+    return sentinel.next;
+}
